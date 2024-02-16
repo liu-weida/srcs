9a2a3efb985413a45a7c64a810b99a0775c79d23
liu-weida <117468158+liu-weida@users.noreply.github.com>
2024-02-16 18:26:34 +0100

    Add files via upload

--- TME5/ressources/srcs/securite/Certif.java
+++ TME5/ressources/srcs/securite/Certif.java
@@ -23,7 +23,6 @@ public class Certif {
 
 
 
-
     public String getId() {
         return id;
     }
--- TME5/ressources/srcs/securite/CertificationAuthority.java
+++ TME5/ressources/srcs/securite/CertificationAuthority.java
@@ -1,4 +1,43 @@
 package srcs.securite;
 
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CertificationAuthority {
+
+    private String nomAlgoAsy;
+    private int tailleCle;
+    private String nomAlgoSign;
+
+    private KeyPair keyPair;
+
+    private Map<String, Certif> certifs = new HashMap<>();
+
+    public CertificationAuthority(String nomAlgoAsy, int tailleCle, String nomAlgoSign) throws NoSuchAlgorithmException {
+        this.nomAlgoAsy = nomAlgoAsy;
+        this.tailleCle = tailleCle;
+        this.nomAlgoSign = nomAlgoSign;
+
+        keyPair = Util.generateNewKeyPair(nomAlgoAsy,tailleCle);
+    }
+
+    public PublicKey getPublicKey(){
+        return keyPair.getPublic();
+    }
+
+    public Certif getCertificate(String idenfifier){
+
+        return null;
+    }
+
+    public Certif declarePublicKey(String identifier, PublicKey pubk)
+            throws GeneralSecurityException{
+
+        return null;
+    }
+
 }
